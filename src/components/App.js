cd8155cddbf495ca327e1d3e7220c7910e5da040
Yves Candau <ycandau@sfu.ca>
2021-10-26 15:50:50 -0700

    Add default light and dark themes using CSS variables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 //------------------------------------------------------------------------------
 // Imports
 
+import { useState, useEffect } from 'react';
+
+import '../css/colors.css';
 import '../css/App.css';
 
 //------------------------------------------------------------------------------
 // Default export
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App = () => {
+  const [dark, setDark] = useState(true);
+
+  useEffect(() => {
+    document.body.dataset.theme = dark ? 'dark' : 'light';
+  }, [dark]);
+
+  return (
+    <main id="app">
+      <section className="header">Header</section>
+      <section className="file-explorer">Text</section>
+      <section className="rest">Rest of the App</section>
+    </main>
+  );
+};
 
 //------------------------------------------------------------------------------
 
