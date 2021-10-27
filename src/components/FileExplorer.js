bf830d2646b87d0b31ef280585533f181042bc9b
Yves Candau <ycandau@sfu.ca>
2021-10-27 00:19:15 -0700

    Filter the file arrays to skip over collapsed folders

--- src/components/FileExplorer.js
+++ src/components/FileExplorer.js
@@ -11,9 +11,16 @@ import '../css/FileExplorer.css';
 const FileExplorer = ({ trees, toggleExpand }) => {
   const files = trees[0];
 
+  const visibleFiles = [];
+  for (let i = 0; i < files.length; ) {
+    const file = files[i];
+    visibleFiles.push(file);
+    i = !file.isDir || file.isExpanded ? i + 1 : file.nextNonChild;
+  }
+
   return (
     <section className="file-explorer">
-      {files.map((fileData) => (
+      {visibleFiles.map((fileData) => (
         <FileEntry
           {...fileData}
           key={fileData.index}
