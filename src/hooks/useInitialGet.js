e24b982ddc3a902bb1dffc4192e8be9868cc4e06
Yves Candau <ycandau@sfu.ca>
2021-10-28 13:35:21 -0700

    Refactor axios hook for initial data fetching

--- src/hooks/useInitialGet.js
+++ src/hooks/useInitialGet.js
@@ -6,17 +6,15 @@ if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
 
-const useInitialGet = (dispatch) => {
+const useInitialGet = async (dispatch) => {
   useEffect(() => {
     dispatch({ type: 'LOADING', payload: 'all' });
     axios
       .get('/api')
-      .then((res) => dispatch({ type: 'GET_TREES', payload: res.data }))
+      .then((res) => dispatch({ type: 'SET_TREES', payload: res.data }))
       .catch((err) => dispatch({ type: 'ERROR', payload: err })); // @todo
     // .finally(() => dispatch({ type: 'FINISHED' }));
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 };
 
-const useAxios = { useInitialGet };
-
-export default useAxios;
+export default useInitialGet;
